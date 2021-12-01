ae18f7f152e7352234a7f7315a5198314c4585bb
Nicholas Brostrom <nicholas.brostrom@effectsoft.se>
2021-12-01 16:13:11 +0100

    Day 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -9,7 +9,41 @@ public class Day_01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var numberedInputs = _input.Split("\n").Select(x => int.Parse(x));
+        var previousMeasurement = numberedInputs.First();
+        var increasedMeasurements = 0;
+        foreach (var measurement in numberedInputs) {
+            if (measurement > previousMeasurement)
+            {
+                increasedMeasurements++;
+            }
+            previousMeasurement = measurement;
+        }
+        return ValueTask.FromResult(increasedMeasurements.ToString());
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        var numberedInputs = _input.Split("\n").Select(x => int.Parse(x));
+        var previousWindowCount = 0;
+        var increasedWindow = 0;
+        var previousWindowIdx = 0;
+        while (true)
+        {
+            var windows = numberedInputs.Skip(previousWindowIdx).Take(3);
+            if (windows.Count() < 3) break;
+            var measuredCount = windows.Sum();
+            if (previousWindowCount != 0)
+            {
+                if (measuredCount > previousWindowCount)
+                    increasedWindow++;
+            }
+            previousWindowCount = measuredCount;
+            previousWindowIdx += 1;
+        }
+
+        return ValueTask.FromResult(increasedWindow.ToString());
+    }
 }
