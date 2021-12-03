4f213bcbc9bb7fda5121dd4c410add25abdca4ef
Nicholas Brostrom <nicholas.brostrom@effectsoft.se>
2021-12-03 09:40:44 +0100

    Day 3 - Part 1

---
+++ AdventOfCode/Day_03.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode;
+
+public class Day_03 : BaseDay
+{
+    private readonly string _input;
+
+    public Day_03()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        //Parse instructions
+        var binaryNumbers = _input.Split("\r\n");
+        var splitNumbers = binaryNumbers.Select(x => x.ToCharArray());
+        var gamma = "";
+        var epsilon = "";
+        var loopCount = splitNumbers.First().Length;
+        for (int i = 0; i < loopCount; i++)
+        {
+            var column = splitNumbers.Select(x => int.Parse(x.ElementAt(i).ToString())).GroupBy(x => x).OrderByDescending(x => x.Count());
+            var mostCommon = column.First().Key;
+            var leastCommon = column.Last().Key;
+            gamma += mostCommon.ToString();
+            epsilon += leastCommon.ToString();
+        }
+        var decimalGamma = Convert.ToInt32(gamma, 2);
+        var decimalEpsilon = Convert.ToInt32(epsilon, 2);
+        return ValueTask.FromResult((decimalGamma * decimalEpsilon).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        //Parse instructions
+        
+        return new ValueTask<string>("");
+    }
+}
