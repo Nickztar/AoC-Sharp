c4ee36a2966dc46c87ed6ef6824725f87ec19f3e
Nicholas Broström <nicholas.brostrom@effectsoft.se>
2021-12-05 16:05:33 +0100

    Day 4 + Day 5

---
+++ AdventOfCode/Day_04.cs
@@ -0,0 +1,130 @@
+﻿namespace AdventOfCode;
+
+public class Day_04 : BaseDay
+{
+    private readonly string _input;
+    public Day_04()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        //Parse instructions
+        var orderAndBoards = _input.Split("\r\n\r\n");
+        var order = orderAndBoards[0].Split(",").Select(int.Parse);
+        var boards = orderAndBoards.Skip(1).Select(b => new Board(b));
+        WinBoard winningBoard = null;
+        for (int numbersToCheck = 1; numbersToCheck <= order.Count(); numbersToCheck++)
+        {
+            if (winningBoard != null) break;
+            var numbers = order.Take(numbersToCheck);
+            foreach (var board in boards)
+            {
+                //Check rows 
+                foreach (var row in board.Rows)
+                {
+                    var marked = row.Where(r => numbers.Any(x => r == x));
+                    if (marked.Count() == 5)
+                    {
+                        //WIN
+                        winningBoard = new WinBoard { Numbers = numbers, Board = board };
+                        break;
+                    }
+                }
+                //Check columns
+                foreach (var column in board.Columns)
+                {
+                    var marked = column.Where(r => numbers.Any(x => r == x));
+                    if (marked.Count() == 5)
+                    {
+                        //WIN
+                        winningBoard = new WinBoard { Numbers = numbers, Board = board };
+                        break;
+                    }
+                }
+                if (winningBoard != null) break;
+            }
+        }
+        var rowNumbers = winningBoard.Board.Rows.SelectMany(x => x);
+        var columnNumbers = winningBoard.Board.Columns.SelectMany(x => x);
+        var boardNumbers = rowNumbers.Concat(columnNumbers).Distinct();
+        var unmarkedNumbers = boardNumbers.Where(x => !winningBoard.Numbers.Contains(x));
+        var sumOfNumbers = unmarkedNumbers.Sum();
+        var result = sumOfNumbers * winningBoard.Numbers.Last();
+        return ValueTask.FromResult(result.ToString());
+    }
+
+    class Board
+    {
+        public Board(string board)
+        {
+            var rows = board.Split("\r\n").Select(x => x.Split(" ").Where(s => s != "").Select(int.Parse).ToList());
+            var columns = new List<IEnumerable<int>>();
+            for (int i = 0; i < 5; i++)
+            {
+                columns.Add(rows.Select(x => x.ElementAt(i)).ToList());
+            }
+            Columns = columns;
+            Rows = rows;
+        }
+        public IEnumerable<IEnumerable<int>> Rows { get; set; }
+        public IEnumerable<IEnumerable<int>> Columns { get; set; }
+        public bool HasWon { get; set; } = false;
+    }
+
+    class WinBoard
+    {
+        public IEnumerable<int> Numbers { get; set; }    
+        public Board Board { get; set; }
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        //Parse instructions
+        var orderAndBoards = _input.Split("\r\n\r\n");
+        var order = orderAndBoards[0].Split(",").Select(int.Parse);
+        var boards = orderAndBoards.Skip(1).Select(b => new Board(b)).ToList();
+        WinBoard winningBoard = null;
+        for (int numbersToCheck = 1; numbersToCheck <= order.Count(); numbersToCheck++)
+        {
+            if (boards.All(b => b.HasWon)) break;
+            var numbers = order.Take(numbersToCheck);
+            var nonWonBoards = boards.Where(x => !x.HasWon).ToList();
+            foreach (var board in nonWonBoards)
+            {
+                //Check rows 
+                foreach (var row in board.Rows)
+                {
+                    var marked = row.Where(r => numbers.Any(x => r == x));
+                    if (marked.Count() == 5)
+                    {
+                        //WIN
+                        board.HasWon = true;
+                        winningBoard = new WinBoard { Numbers = numbers, Board = board };
+                        break;
+                    }
+                }
+                //Check columns
+                foreach (var column in board.Columns)
+                {
+                    var marked = column.Where(r => numbers.Any(x => r == x));
+                    if (marked.Count() == 5)
+                    {
+                        //WIN
+                        board.HasWon = true;
+                        winningBoard = new WinBoard { Numbers = numbers, Board = board };
+                        break;
+                    }
+                }
+            }
+        }
+        var rowNumbers = winningBoard.Board.Rows.SelectMany(x => x);
+        var columnNumbers = winningBoard.Board.Columns.SelectMany(x => x);
+        var boardNumbers = rowNumbers.Concat(columnNumbers).Distinct();
+        var unmarkedNumbers = boardNumbers.Where(x => !winningBoard.Numbers.Contains(x));
+        var sumOfNumbers = unmarkedNumbers.Sum();
+        var result = sumOfNumbers * winningBoard.Numbers.Last();
+        return ValueTask.FromResult(result.ToString());
+    }
+}
---
+++ AdventOfCode/Day_05.cs
@@ -0,0 +1,151 @@
+﻿namespace AdventOfCode;
+
+public class Day_05 : BaseDay
+{
+    private readonly string _input;
+    public Day_05()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        //Parse instructions
+        var lines = _input.Split("\r\n");
+        var ranges = lines
+            .Select(x => 
+                x.Split(" -> ")
+                .Select(p => 
+                    p.Split(",")
+                        .Select(int.Parse)
+                )
+            );
+        var points = new Dictionary<(int x, int y), int>();
+        foreach (var line in ranges)
+        {
+            var first = line.First();
+            var x1 = first.First();
+            var y1 = first.Last();
+            var last = line.Last();
+            var x2 = last.First();
+            var y2 = last.Last();
+            //Only check horizontal and vertical lines
+            if (x1 == x2)
+            {
+                //Generate the position in the range
+                for (int number = Math.Min(y1, y2); number <= Math.Max(y1, y2); number++)
+                {
+                    var pos = (x1, number);
+                    if (points.TryGetValue(pos, out int count))
+                    {
+                        points[pos] = count + 1;
+                    }
+                    else
+                    {
+                        points[pos] = 1;
+                    }
+                }
+            }
+            else if (y1 == y2)
+            {
+                //Generate the position in the range
+                for (int number = Math.Min(x1, x2); number <= Math.Max(x1, x2); number++)
+                {
+                    var pos = (number, y1);
+                    if (points.TryGetValue(pos, out int count))
+                    {
+                        points[pos] = count + 1;
+                    }
+                    else
+                    {
+                        points[pos] = 1;
+                    }
+                }
+            }
+        }
+        var result = points.Count(x => x.Value > 1);
+        return ValueTask.FromResult(result.ToString());
+    }
+    public override ValueTask<string> Solve_2()
+    {
+        //Parse instructions
+        var lines = _input.Split("\r\n");
+        var ranges = lines
+            .Select(x =>
+                x.Split(" -> ")
+                .Select(p =>
+                    p.Split(",")
+                        .Select(int.Parse)
+                )
+            );
+        var points = new Dictionary<(int x, int y), int>();
+        void AddPosition((int x, int y) pos)
+        {
+            if (points.TryGetValue(pos, out int count))
+            {
+                points[pos] = count + 1;
+            }
+            else
+            {
+                points[pos] = 1;
+            }
+        }
+        foreach (var line in ranges)
+        {
+            var first = line.First();
+            var x1 = first.First();
+            var y1 = first.Last();
+            var last = line.Last();
+            var x2 = last.First();
+            var y2 = last.Last();
+            //Only check horizontal and vertical lines
+            if (x1 == x2)
+            {
+                //Vertical
+                for (int number = Math.Min(y1, y2); number <= Math.Max(y1, y2); number++)
+                {
+                    var pos = (x1, number);
+                    AddPosition(pos);
+                }
+            }
+            else if (y1 == y2)
+            {
+                //Horizontal
+                for (int number = Math.Min(x1, x2); number <= Math.Max(x1, x2); number++)
+                {
+                    var pos = (number, y1);
+                    AddPosition(pos);
+                }
+            }
+            else
+            {
+                //Generate all position diagonal
+                if (x1 > x2)
+                {
+                    (x1, y1, x2, y2) = (x2, y2, x1, y1);
+                }
+
+                if (y1 < y2)
+                {
+                    //decending diagonal
+                    for (int number = 0; number < x2 + 1 - x1; number++)
+                    {
+                        var pos = (x1 + number, y1 + number);
+                        AddPosition(pos);
+                    }
+                }
+                else
+                {
+                    //ascending diagonal
+                    for (int number = 0; number < x2 + 1 - x1; number++)
+                    {
+                        var pos = (x1 + number, y1 - number);
+                        AddPosition(pos);
+                    }
+                }
+            }
+        }
+        var result = points.Count(x => x.Value > 1);
+        return ValueTask.FromResult(result.ToString());
+    }
+}
